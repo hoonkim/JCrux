452b93c3fbba4af005eb310199fe221563145c58
hoonkim <hkim@ihateflyingbugs.com>
2014-06-14 01:47:37 +0900

    첫 커밋 다시.

---
+++ JCrux/src/edu/hyu/cs/jcrux/CruxMain.java
@@ -0,0 +1,13 @@
+package edu.hyu.cs.jcrux;
+
+/**
+ * The starting point for crux. Prints a general usage statement when given no
+ * arguments. Runs one of the crux commands, including printing the current
+ * version number.
+ * 
+ * @author HoonKim
+ * 
+ */
+public class CruxMain {
+	
+}
